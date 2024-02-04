2b7cb356631f54ac0501fd7ca419bfd537abfbc6
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-02-04 15:35:19 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const userModel = require('./models/userModel')
 const bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 
+const createUser = require ('./usersCreation');
+const createProduct = require ('./productsCreation');
+const showProductsList = require ('./ShowProductsList');
+const showProductsListByType = require ('./ShowProductsListByType');
 
 connectDb()
 const app = express()
@@ -21,79 +25,7 @@ function listAllUsers(req, res) {
   res.send('teste11')
 }
 
-async function createUser(req, res) {
-  const passwordHash = await bcrypt.hash(req.body.password, 10)
-  const input = {
-    name: req.body.name,
-    document: req.body.document,
-    email: req.body.email,
-    password: passwordHash,
-    phone: req.body.phone
-  }
 
-
-  if (input.name.length < 2) {
-    res.send('Preencha seu nome')
-    return app.post('/users/create', createUser)
-  }
-  const userDocument = await userModel.findOne({ document: input.document })
-  if (userDocument) {
-    res.send('CPF já cadastrado')
-    return
-  }
-  if (isNaN(input.document)) {
-    res.send('Insira apenas números no seu CPF')
-    return
-  }
-  if (input.document.length < 11) {
-    res.send('O CPF está incorreto, preencha novamente menor')
-    return
-  }
-  if (input.document.length > 11) {
-    res.send('O CPF está incorreto, preencha novamente maior')
-    return
-  }
-  //userModel (arquivo) .findOne ({primeiroParam é o atributo do schema/banco (email, name, document), 
-  //segundoParam é o atributo do objeto input})
-  const userEmail = await userModel.findOne({ email: input.email })
-  if (userEmail) {
-    res.send('E-mail já cadastrado')
-    return
-  }
-  if ((input.email.indexOf("@") == -1) || (input.email.indexOf(".") == - 1) || (input.email.length < 10)) {
-    res.send('Insira um e-mail válido e completo')
-    return
-  }
-  // if (input.email == userModel.search(input.email)) {
-  //   res.send('O e-mail já está em uso')
-  //   createUser()
-  // }
-  if (req.body.password.length < 6) {
-    res.send('Crie uma senha com pelo menos 6 digitos')
-    return
-  }
-  const userPhone = await userModel.findOne({ phone: input.phone })
-  if (userPhone) {
-    res.send('Telefone já cadastrado')
-    return
-  }
-  if (isNaN(input.phone)) {
-    res.send('Insira apenas números no seu telefone')
-    return
-  }
-  if (input.phone.length < 11) {
-    res.send('Insira seu telefone completo incluindo o DDD como no exemplo: xx9xxxxXXXX')
-    return
-  }
-  // if (input.document == userModel.search(input.document)) {
-  //   res.send('O CPF já está em uso')
-  //   createUser()
-  // }
-
-  await userModel.create(input)
-  res.send(input)
-
-}
 
 async function loginAccess(req, res) {
   const { email, password } = req.body
@@ -146,9 +78,11 @@ function verifyToken(req, res, next) {
 
 
 app.post('/users/create', createUser)
+app.post('/products/create', createProduct)
 app.post('/users/login', loginAccess)
 app.post('/users/list', verifyToken, listAllUsers)
-
+app.get('/products/list', showProductsList)
+app.get('/products/list/by-type', showProductsListByType)
 
 
 
