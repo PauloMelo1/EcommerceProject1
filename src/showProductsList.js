d454ea5eecf7618647336acc353ecf8db3965b9e
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-02-04 15:36:43 -0300

    Update showProductsList.js

--- src/showProductsList.js
+++ src/showProductsList.js
@@ -1,1 +1,29 @@
 
+const connectDb = require('./mongoDb')
+const express = require('express')
+const productsModel = require('./models/productsModel')
+const userModel = require('./models/userModel')
+
+
+connectDb()
+const app = express()
+app.use(express.json());
+
+
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+async function showProductsList(req, res) {
+
+    const response = await productsModel.find().select({
+        inventory: 0,
+        product: 0,
+        characteristics: 0,
+        stamp: 0,
+        group: 0
+    });
+    res.send(response);
+
+}
+module.exports = showProductsList
